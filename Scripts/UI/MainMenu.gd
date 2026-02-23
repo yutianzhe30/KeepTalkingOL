b60d2f61b0ca1b513a21d8d8ef5c2df2491f25ec
tianzhe yu <tianzhe.yu@ifak.eu>
2026-02-23 12:25:08 +0100

    new version deploy

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -4,9 +4,18 @@ func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_manual_button_pressed() -> void:
-	var url = "https://www.google.com"
+	# Assume the PDF is placed directly next to the exported executable or HTML file
+	var pdf_filename = "MANUAL_CN.pdf"
+	
 	if OS.has_feature("web"):
-		var js_code = "window.open('%s', '_blank');" % url
+		# On a web server, a relative path opens relative to the index.html
+		var js_code = "window.open('%s', '_blank');" % pdf_filename
 		JavaScriptBridge.eval(js_code)
+	elif OS.has_feature("editor"):
+		# When playing from the Godot Editor, look for it in the project root
+		OS.shell_open(ProjectSettings.globalize_path("res://" + pdf_filename))
 	else:
-		OS.shell_open(url)
+		# For exported PC builds (Windows/Mac/Linux), find it next to the .exe
+		var exec_dir = OS.get_executable_path().get_base_dir()
+		var pdf_path = exec_dir.path_join(pdf_filename)
+		OS.shell_open(pdf_path)
